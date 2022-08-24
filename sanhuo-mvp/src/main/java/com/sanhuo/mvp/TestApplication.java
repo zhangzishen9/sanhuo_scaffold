ffe8d3fcbcf695b0fbf1f73abe32326150f27107
张梓燊 <zhangzishen@cvte.com>
2022-08-24 14:10:05 +0800

    feat: 调整架构

--- sanhuo-mvp/src/main/java/com/sanhuo/mvp/TestApplication.java
+++ sanhuo-mvp/src/main/java/com/sanhuo/mvp/TestApplication.java
@@ -1,21 +1,32 @@
-package com.sanhuo.test;
+package com.sanhuo.mvp;
 
-import com.sanhuo.persistent.EnableSanHuoPersistent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Map;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class})
-@EnableEurekaClient
-@ComponentScan("com.sanhuo.**")
-@EnableSanHuoPersistent("com.sanhuo.test.mapper")
+@Slf4j
 public class TestApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-
+        TcpServer server = new TcpServer();
+        server.startSocketServer(8907);
     }
+
 
 }
